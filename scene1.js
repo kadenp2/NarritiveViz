c51d8bbbc943aca7d15a80cd70756ffe3db7daba
kadenp2 <143836658+kadenp2@users.noreply.github.com>
2024-07-28 15:30:51 -0500

    Update scene1.js

--- scene1.js
+++ scene1.js
@@ -1,1 +1,57 @@
+// Scene 1: Global CO2 Emissions Over Time
+d3.csv("C02emissions.csv").then(data => {
+    // Parse the data
+    data.forEach(d => {
+        d.Year = +d.Year;
+        d.CO2 = +d.CO2;
+    });
 
+    // Set up the SVG canvas dimensions
+    const margin = { top: 20, right: 30, bottom: 40, left: 50 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    const svg = d3.select("#scene1").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + margin.top + ")");
+
+    // Set up scales
+    const x = d3.scaleTime()
+        .domain(d3.extent(data, d => d.Year))
+        .range([0, width]);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.CO2)])
+        .nice()
+        .range([height, 0]);
+
+    // Add the x-axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // Add the y-axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // Add the line
+    svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+            .x(d => x(d.Year))
+            .y(d => y(d.CO2))
+        );
+
+    // Add title
+    svg.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 0 - (margin.top / 2))
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .text("Global CO2 Emissions Over Time");
+});
