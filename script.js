2a6e3ce142b3fda965df3f4d30d1ae93301cefaf
kadenp2 <143836658+kadenp2@users.noreply.github.com>
2024-07-28 17:30:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Initialize global variables
 var sceneId = 0;
-var dataPath = 'CO2emissions.csv';
+var dataPath = '';
+var co2DataPath = 'CO2emissions.csv';
+var lifeExpectancyDataPath = 'life-expectancy.csv';
 
 function homeScene() {
     sceneId = 0;
@@ -44,7 +46,17 @@ function clearChart() {
 }
 
 function loadChart(sceneId) {
-    d3.csv(dataPath).then(function (data) {
+    if (sceneId % 2 === 1) {
+        dataPath = co2DataPath;
+        loadCO2Chart();
+    } else {
+        dataPath = lifeExpectancyDataPath;
+        loadLifeExpectancyChart();
+    }
+}
+
+function loadCO2Chart() {
+    d3.csv(dataPath).then(function(data) {
         data.forEach(d => {
             d.Year = +d.Year;
             d['Annual CO₂ emissions'] = +d['Annual CO₂ emissions'];
@@ -116,6 +128,81 @@ function loadChart(sceneId) {
     });
 }
 
+function loadLifeExpectancyChart() {
+    d3.csv(dataPath).then(function(data) {
+        data.forEach(d => {
+            d.Year = +d.Year;
+            d['Life expectancy'] = +d['Life expectancy'];
+        });
+
+        var nestedData = d3.group(data, d => d.Entity);
+
+        const margin = { top: 20, right: 80, bottom: 50, left: 50 },
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        const svg = d3.select("#chart")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        const x = d3.scaleTime()
+            .domain(d3.extent(data, d => d.Year))
+            .range([0, width]);
+
+        const y = d3.scaleLinear()
+            .domain([0, d3.max(data, d => d['Life expectancy'])])
+            .nice()
+            .range([height, 0]);
+
+        const line = d3.line()
+            .x(d => x(d.Year))
+            .y(d => y(d['Life expectancy']));
+
+        svg.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        nestedData.forEach((values, key) => {
+            svg.append("path")
+                .datum(values)
+                .attr("fill", "none")
+                .attr("stroke", d3.schemeCategory10[Math.floor(Math.random() * 10)])
+                .attr("stroke-width", 1.5)
+                .attr("d", line);
+        });
+
+        const legend = svg.selectAll(".legend")
+            .data(nestedData.keys())
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", (d, i) => `translate(0,${i * 20})`);
+
+        legend.append("rect")
+            .attr("x", width - 18)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", d3.schemeCategory10[Math.floor(Math.random() * 10)]);
+
+        legend.append("text")
+            .attr("x", width - 24)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "end")
+            .text(d => d);
+
+    }).catch(error => {
+        console.error("Error loading the data: ", error);
+    });
+}
+
 document.getElementById("bh").addEventListener("click", homeScene);
 document.getElementById("bp").addEventListener("click", previousScene);
 document.getElementById("bn").addEventListener("click", nextScene);
+
+window.onload = initVisualization;
