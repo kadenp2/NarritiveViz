8218d875887a9f7fa6f0e7fc0c567574af39eea7
kadenp2 <143836658+kadenp2@users.noreply.github.com>
2024-07-28 14:37:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,89 +1,203 @@
-// Load data
+// script.js
+
+// Load the datasets
 Promise.all([
-    d3.csv('C02emissions.csv'),
-    d3.csv('obesitydata.csv'),
-    d3.csv('life-expectancy.csv')
-]).then(function(files) {
-    const co2Data = files[0];
-    const obesityData = files[1];
-    const lifeExpectancyData = files[2];
-
-    // Parse data
-    co2Data.forEach(d => {
-        d.Year = +d.Year;
-        d.Emissions = +d.Emissions;
-    });
-
-    obesityData.forEach(d => {
-        d.Year = +d.Year;
-        d.ObesityRate = +d.ObesityRate;
-    });
-
+    d3.csv('/mnt/data/life-expectancy.csv'),
+    d3.csv('/mnt/data/obesitydata.csv')
+]).then(([lifeExpectancyData, obesityData]) => {
+    // Preprocess data
     lifeExpectancyData.forEach(d => {
         d.Year = +d.Year;
         d.LifeExpectancy = +d.LifeExpectancy;
     });
 
-    // Define dimensions and margins
-    const margin = {top: 20, right: 20, bottom: 30, left: 40},
-          width = 960 - margin.left - margin.right,
-          height = 500 - margin.top - margin.bottom;
+    obesityData.forEach(d => {
+        d.ObesityRate = +d.ObesityRate;
+    });
+
+    // Initial visualization parameters
+    const width = 800;
+    const height = 600;
+    const margin = { top: 50, right: 50, bottom: 50, left: 50 };
 
     // Create SVG container
-    const svg = d3.select("#visualization").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    const svg = d3.select('#visualization')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
 
-    // Define scales
-    const x = d3.scaleLinear()
-        .domain([d3.min(co2Data, d => d.Year), d3.max(co2Data, d => d.Year)])
-        .range([0, width]);
+    // Scene 1: Life Expectancy over Time
+    function scene1() {
+        svg.selectAll('*').remove(); // Clear previous scene
 
-    const y = d3.scaleLinear()
-        .range([height, 0]);
+        // Define scales
+        const x = d3.scaleTime()
+            .domain(d3.extent(lifeExpectancyData, d => d.Year))
+            .range([margin.left, width - margin.right]);
 
-    // Define axes
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisLeft(y);
+        const y = d3.scaleLinear()
+            .domain([0, d3.max(lifeExpectancyData, d => d.LifeExpectancy)])
+            .nice()
+            .range([height - margin.bottom, margin.top]);
 
-    // Add axes to SVG
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        // Define axes
+        const xAxis = d3.axisBottom(x);
+        const yAxis = d3.axisLeft(y);
 
-    const yAxisGroup = svg.append("g")
-        .attr("class", "y axis");
+        // Append axes
+        svg.append('g')
+            .attr('transform', `translate(0,${height - margin.bottom})`)
+            .call(xAxis);
 
-    // Add line
-    const line = svg.append("path")
-        .attr("class", "line")
-        .style("fill", "none")
-        .style("stroke-width", "2px");
+        svg.append('g')
+            .attr('transform', `translate(${margin.left},0)`)
+            .call(yAxis);
 
-    // Function to update the line
-    function updateLine(data, yValue, color) {
-        y.domain([0, d3.max(data, yValue)]);
-        yAxisGroup.transition().duration(1000).call(yAxis);
-
-        const lineFunction = d3.line()
+        // Line generator
+        const line = d3.line()
             .x(d => x(d.Year))
-            .y(d => y(yValue(d)));
+            .y(d => y(d.LifeExpectancy));
 
-        line.datum(data)
-            .transition()
-            .duration(1000)
-            .attr("d", lineFunction)
-            .style("stroke", color);
+        // Append line
+        svg.append('path')
+            .datum(lifeExpectancyData)
+            .attr('fill', 'none')
+            .attr('stroke', 'steelblue')
+            .attr('stroke-width', 1.5)
+            .attr('d', line);
+
+        // Append title
+        svg.append('text')
+            .attr('x', width / 2)
+            .attr('y', margin.top / 2)
+            .attr('text-anchor', 'middle')
+            .attr('font-size', '16px')
+            .attr('font-weight', 'bold')
+            .text('Life Expectancy Over Time');
+    }
+
+    // Scene 2: Obesity Rates by Country
+    function scene2() {
+        svg.selectAll('*').remove(); // Clear previous scene
+
+        // Define scales
+        const x = d3.scaleBand()
+            .domain(obesityData.map(d => d.Country))
+            .range([margin.left, width - margin.right])
+            .padding(0.1);
+
+        const y = d3.scaleLinear()
+            .domain([0, d3.max(obesityData, d => d.ObesityRate)])
+            .nice()
+            .range([height - margin.bottom, margin.top]);
+
+        // Define axes
+        const xAxis = d3.axisBottom(x);
+        const yAxis = d3.axisLeft(y);
+
+        // Append axes
+        svg.append('g')
+            .attr('transform', `translate(0,${height - margin.bottom})`)
+            .call(xAxis)
+            .selectAll('text')
+            .attr('transform', 'rotate(-45)')
+            .style('text-anchor', 'end');
+
+        svg.append('g')
+            .attr('transform', `translate(${margin.left},0)`)
+            .call(yAxis);
+
+        // Append bars
+        svg.selectAll('.bar')
+            .data(obesityData)
+            .enter().append('rect')
+            .attr('class', 'bar')
+            .attr('x', d => x(d.Country))
+            .attr('y', d => y(d.ObesityRate))
+            .attr('width', x.bandwidth())
+            .attr('height', d => height - margin.bottom - y(d.ObesityRate))
+            .attr('fill', 'steelblue');
+
+        // Append title
+        svg.append('text')
+            .attr('x', width / 2)
+            .attr('y', margin.top / 2)
+            .attr('text-anchor', 'middle')
+            .attr('font-size', '16px')
+            .attr('font-weight', 'bold')
+            .text('Obesity Rates by Country');
+    }
+
+    // Scene 3: Correlation between Life Expectancy and Obesity
+    function scene3() {
+        svg.selectAll('*').remove(); // Clear previous scene
+
+        // Combine data based on country
+        const combinedData = obesityData.map(ob => {
+            const lifeExp = lifeExpectancyData.find(le => le.Country === ob.Country);
+            return {
+                Country: ob.Country,
+                ObesityRate: ob.ObesityRate,
+                LifeExpectancy: lifeExp ? lifeExp.LifeExpectancy : null
+            };
+        }).filter(d => d.LifeExpectancy !== null);
+
+        // Define scales
+        const x = d3.scaleLinear()
+            .domain([0, d3.max(combinedData, d => d.ObesityRate)])
+            .nice()
+            .range([margin.left, width - margin.right]);
+
+        const y = d3.scaleLinear()
+            .domain([0, d3.max(combinedData, d => d.LifeExpectancy)])
+            .nice()
+            .range([height - margin.bottom, margin.top]);
+
+        // Define axes
+        const xAxis = d3.axisBottom(x);
+        const yAxis = d3.axisLeft(y);
+
+        // Append axes
+        svg.append('g')
+            .attr('transform', `translate(0,${height - margin.bottom})`)
+            .call(xAxis);
+
+        svg.append('g')
+            .attr('transform', `translate(${margin.left},0)`)
+            .call(yAxis);
+
+        // Append scatter plot
+        svg.selectAll('.dot')
+            .data(combinedData)
+            .enter().append('circle')
+            .attr('class', 'dot')
+            .attr('cx', d => x(d.ObesityRate))
+            .attr('cy', d => y(d.LifeExpectancy))
+            .attr('r', 5)
+            .attr('fill', 'steelblue');
+
+        // Append title
+        svg.append('text')
+            .attr('x', width / 2)
+            .attr('y', margin.top / 2)
+            .attr('text-anchor', 'middle')
+            .attr('font-size', '16px')
+            .attr('font-weight', 'bold')
+            .text('Correlation between Life Expectancy and Obesity');
+    }
+
+    // Define parameters and triggers
+    let currentScene = 0;
+    const scenes = [scene1, scene2, scene3];
+
+    function showNextScene() {
+        currentScene = (currentScene + 1) % scenes.length;
+        scenes[currentScene]();
     }
 
     // Initial scene
-    updateLine(co2Data, d => d.Emissions, "steelblue");
+    scenes[currentScene]();
 
-    // Event listeners for buttons
-    d3.select("#scene1").on("click", () => updateLine(co2Data, d => d.Emissions, "steelblue"));
-    d3.select("#scene2").on("click", () => updateLine(obesityData, d => d.ObesityRate, "orange"));
-    d3.select("#scene3").on("click", () => updateLine(lifeExpectancyData, d => d.LifeExpectancy, "green"));
+    // Trigger to show the next scene
+    d3.select('body').on('click', showNextScene);
 });
